f2f874c9ccfe10e2c2fe2475966efbb422048225
rizky-hub <59041420+rizky-hub@users.noreply.github.com>
2020-08-24 21:14:15 +0700

    added rest api

---
+++ database/migrations/2020_08_24_111607_create_flights_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->increments('code');
+            $table->string('flightNumber');
+            $table->string('departurePort');
+            $table->string('arrivalPort');
+            $table->dateTime('departureTime');
+            $table->dateTime('arrivalTime');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
